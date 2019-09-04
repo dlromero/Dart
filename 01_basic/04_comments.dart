0bdf70dc4a8c0606e9fe1d58c5a1069392fbdef0
Daniel <raiga1234@gmail.com>
2019-09-03 20:31:02 -0500

    Comments

---
+++ 01_basic/04_comments.dart
@@ -0,0 +1,29 @@
+/**
+ * Este archivo sirve para explicar los comentarios en dart
+ * 
+ * [creadoEn]: Hoy
+ */
+///
+///
+main() {
+  // Esta es una vairable temporal
+  int a = 10; // Deberia ser 10
+
+  /*
+  final personas = [
+    // Esta es la primera persona
+    'Fernando', // [0]
+    //Esta es la segunda persona
+    'Pedro', // [1]
+    //Esta es la segunda persona
+    'Carlos' // [2]
+  ];
+  */
+}
+
+// Nombre: cualquier comentario
+// Mensaje: Saludo cualquier comentario
+
+/// Funciona para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar(int nombre, int mensaje) {}

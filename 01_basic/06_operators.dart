88bb3f3da533dd4d5497ec2165d7fec55b93e485
Daniel <raiga1234@gmail.com>
2019-09-03 20:54:48 -0500

    Asignation operators, conditional, relational and type

--- 01_basic/06_operators.dart
+++ 01_basic/06_operators.dart
@@ -9,4 +9,29 @@ main() {
   int c = 23;
   String resp = c > 25 ? 'C es mayor de 25' : 'C es menor a 25';
   int d = b ?? a ?? 100;
+
+  // Operadores Relacionales
+  // Todos retornan un valor booleano
+  /*
+    > Mayor que
+    < Menos que
+    >= Mayor o igual que
+    <= Menor o igual que
+
+    == Revisa si dos objetos son iguales
+    != Revisa su ds objetos si son diferentes
+
+   */
+
+  String persona1 = 'Fernando';
+  String persona2 = 'Alberto';
+
+  int x = 20;
+  int y = 30;
+
+  // Operador de tipo
+  int i = 10;
+  String j = '10';
+  print(i is int);
+  print(j is! int);
 }

57558fbea76201dc3122618bd6a87f5e6f97c298
Daniel <raiga1234@gmail.com>
2019-09-03 20:40:50 -0500

    Aritmetic operators

---
+++ 01_basic/05_operators_math.dart
@@ -0,0 +1,20 @@
+/**
+ *  Un operador es un simbolo que le dice al compilador
+ *  que debe realizar una tarea matematica, relacional o logica
+ *  y debe producir un resultado
+ */
+main() {
+  int a = 10 + 5; // + 15
+  a = 20 - 10; // - 10
+  a = 10 * 2; // * 20
+  double b = 10 / 2; // / 5
+  b = 10.0 % 3; // % 1 El sobrante de la división
+  b = -b; // -expr Es usado para cambiar el valor de la expresion
+  int c = 10 ~/ 3; // 3 ~/ Division normal pero solo la pate entera
+  int d = 1;
+  d++; // ++ 2
+  d--; // -- 1
+
+  d += 2; // += 3
+  d -= 2; // -= 1  *= /=
+}

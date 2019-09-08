e26256b567dc262ec391cda2c9e40fb3f38a6d6f
Daniel R <raiga1234@gmail.com>
2019-09-08 17:20:50 -0500

    Enums in Dart

---
+++ 04_noncommon_types/02_enums.dart
@@ -0,0 +1,17 @@
+main() {
+  // int volumen = 1;
+  Audio volumen = Audio.alto;
+  switch (volumen) {
+    case Audio.bajo:
+      print('Volumen bajo');
+      break;
+    case Audio.medio:
+      print('Volumen medio');
+      break;
+    case Audio.alto:
+      print('Volumen alto');
+      break;
+  }
+}
+
+enum Audio { bajo, medio, alto }

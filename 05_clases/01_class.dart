faf34834caebb79c2bf5fede7ebf2b833c8846da
Daniel R <raiga1234@gmail.com>
2019-09-15 18:51:21 -0500

    Set properties dart

--- 05_clases/01_class.dart
+++ 05_clases/01_class.dart
@@ -1,27 +1,10 @@
-import 'dart:developer';
+import 'clases/persona.dart';
 
 main() {
-  // final persona = {'nombre': 'Daniel', 'edad': 33};
   final persona = new Persona();
-  persona.nombre = 'Daniel';
-  persona.edad = 33;
-  persona.bio = 'Colombia';
-  print(persona.toString());
-}
-
-class Persona {
-  // Campos o prpiedades
-  String nombre;
-  int edad;
-  String bio;
-
-  // Get y sets
-
-  // Constructores
-
-  // metodos
-  @override
-  String toString() {
-    return '$nombre';
-  }
+  persona
+    ..nombre = 'Daniel'
+    ..edad = 33
+    ..bio = 'Colombia';
+  print(persona);
 }
---
+++ 05_clases/clases/persona.dart
@@ -0,0 +1,16 @@
+class Persona {
+  // Campos o prpiedades
+  String nombre;
+  int edad;
+  String bio;
+
+  // Get y sets
+
+  // Constructores
+
+  // metodos
+  @override
+  String toString() {
+    return '$nombre';
+  }
+}

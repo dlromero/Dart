f0542f6d2009626f39ec7d1107b1e3ba70a1cfd0
Romero Albarracin <s4945948@bns.bns>
2019-09-16 17:34:00 -0500

    Documentation Dart

---
+++ 07_docs/01_double.dart
@@ -0,0 +1,19 @@
+main() {
+  double numero = 3.1416;
+  double infinito = double.infinity;
+
+  print('Firma: ${numero.sign} :: $numero');
+
+  print('isFinite: ${numero.isFinite} :: $numero');
+  print('isFinite: ${infinito.isFinite} :: $infinito');
+  print('abs: ${numero.abs()} :: $numero');
+  print('ceil: ${numero.ceil()} :: $numero');
+  // print('ceil: ${infinito.ceil()} :: $numero');
+
+  print('ceilToDouble: ${numero.ceilToDouble()} :: $numero');
+
+  print('round: ${numero.round()} :: $numero');
+  print('round: ${numero.roundToDouble()} :: $numero');
+
+  print('clamp: ${numero.clamp(1, 4)} :: $numero');
+}

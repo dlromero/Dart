3f37233d0149380a1bd9226662ab49a602c20839
Daniel R <raiga1234@gmail.com>
2019-09-15 21:39:59 -0500

    super constructor in Dart

---
+++ 06_classes_adv/03_super_constructor.dart
@@ -0,0 +1,18 @@
+class Persona {
+  String nombre;
+  int edad;
+  Persona(this.nombre, this.edad);
+  void imprimirNombre() => print('Nombre: $nombre, Edad: $edad');
+}
+
+class Cliente extends Persona {
+  String direccion;
+  List ordenes = [];
+
+  Cliente(String nombre, int edad) : super(nombre, edad);
+}
+
+main() {
+  final pedro = new Cliente('Daniel', 44);
+  pedro.imprimirNombre();
+}

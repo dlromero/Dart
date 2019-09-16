449c7d316f0a767494da62c7ff016fb659be858e
Daniel Romero <raiga1234@gmail.com>
2019-09-16 18:11:11 -0500

    Operators dart

--- 07_docs/02_string.dart
+++ 07_docs/02_string.dart
@@ -5,7 +5,23 @@ main(List<String> args) {
 
   print('String: $nombreCompleto');
   print('Length: ${nombreCompleto.length}');
-  print('Contains D: ${nombreCompleto.contains('D',0)}');
+  print('Contains D: ${nombreCompleto.contains('D', 0)}');
   print('Endwith o: ${nombreCompleto.endsWith('o')}');
-  print('PadLeft : ${nombreCompleto.padLeft(20,'...')}');
+  print('PadLeft : ${nombreCompleto.padLeft(20, '...')}');
+  print('PadRight : ${nombreCompleto.padRight(20, '...')}');
+
+  print('Operador [] : ${nombreCompleto[0]}');
+  print('Operador * : ${nombreCompleto * 2}');
+  print('Operador * : ${'*' * 10}');
+
+  print(
+      'ReplaceAll replace all : ${nombreCompleto.replaceAll(new RegExp(r'e'), 'd')}');
+
+  print('SubString : ${nombreCompleto.substring(0, 5)} ...');
+  print('indexOf D: ${nombreCompleto.indexOf('D')} '); // Daniel
+
+  print('Split D: ${nombreCompleto.split(' ')} '); // Daniel
+  print('Split_ ${nombreCompleto.split(' ')[1]}');
+
+  print('Capitalizar ${nombreCompleto[nombreCompleto.length - 1]}');
 }
